017bacac1daffbe15fdb16db9a66f6b3593bc157
Mia Nguyen <nguyendanlinh9122000@gmail.com>
2023-03-24 11:46:35 -0700

    set up database for main url localhost:3000 and dynamic url http://localhost:3000/customeListName, also handle upper/lowercase of customListName by using Lodash and change all to capitalized

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
-
+//set up
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("Lodash");
 
 const app = express();
 
@@ -10,38 +11,132 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+//------------------------------
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+//connect to database
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model("Item", itemsSchema); //first param is singular version of collection name
+
+const item1 = new Item({
+  name: "Welcome"
+})
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+})
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+})
+const defaultItems = [item1, item2, item3];
+
+
+
+const ListSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+const List = mongoose.model("List", ListSchema);  //create other collection
+
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+
+//-----------------------------
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
+  const day = date.getDate();
+  
+  Item.find({}).then((items)=>{  //find all, items is returned as an array
+    if(items.length === 0 ){  //only add the default items once, when there's nothing in list yet
+      Item.insertMany(defaultItems);
+      res.redirect("/");  //redirect to root when there're already default items in list
+    } else {
+      res.render("list", {listTitle: day, newListItems: items});
 
-  res.render("list", {listTitle: day, newListItems: items});
+    }
+  })
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem; //in list.ejs, <input> has name "newItem"
+  const listName = req.body.list; //in list.ejs, <button> has name "list"
+  
+  const item = new Item({
+    name: itemName
+  })
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+  if(listName === date){
+    item.save();
     res.redirect("/");
+  } else {  //for dynamic url, listName is customeListName
+    List.findOne({name: listName}).then((foundList)=>{
+      // List.updateOne({name: foundList.name}, {$push: {Items: item}}).then(()=>{
+      //   //foundList.save();
+      // })
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    })
   }
+  
+
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+app.post("/delete", function(req, res){   //in list.ejs, when the checkbox is checked, it'll submit a post request to /delete
+  const checkedItemId = req.body.checkbox;  //req.body.checkbox is the id of the checked item
+  const listName = req.body.listName; //in list.ejs, <input type="hidden"> has name="listName" 
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
+  if(listName === date){
+    Item.findByIdAndRemove(checkedItemId).then(()=>{});
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}).then(()=>{
+      res.redirect("/" + listName);
+    });
+  }
+
+  
+})
+
+//---------------------------
+
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
+
+// app.get("/about", function(req, res){
+//   res.render("about");
+// });
+
+app.get("/:customListName", function(req, res){ //dynamic url "/:params"
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}).then((foundList)=>{
+    if (!foundList) {  //if foundList hasn't exist, create a new list collection
+      const list = new List({
+        name: customListName,
+        items: defaultItems
+      });
+      list.save();
+      res.redirect("/" + customListName);
+    } else { 
+      res.render("List", {listTitle: foundList.name, newListItems: foundList.items});
+    }
+  });
+})
+
+//-----------------------------
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
